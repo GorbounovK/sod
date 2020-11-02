eef05abbc662060b751efd7770165851dec9fb29
Gorbounov Konstantin <gorbounov.k@gmail.com>
2020-11-02 20:54:03 +0200

    добавил настройку Executor, ограничил 2 потоками

--- src/main/java/ua/org/gorbounov/DemoApplication.java
+++ src/main/java/ua/org/gorbounov/DemoApplication.java
@@ -1,5 +1,7 @@
 package ua.org.gorbounov;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -22,6 +25,9 @@ public class DemoApplication {
         log.info("=================================");
         log.info("Helo SOD!");
         log.info("------- Initialization Complete -----------");
+        log.debug("-------debug Initialization Complete -----------");
+        log.trace("-------trace Initialization Complete -----------");
+        log.error("-------error Initialization Complete -----------");
 	}
 
 	@Bean
@@ -38,4 +44,16 @@ public class DemoApplication {
 
 		};
 	}
+	
+	  @Bean
+	  public Executor taskExecutor() {
+	    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+	    executor.setCorePoolSize(2);
+	    executor.setMaxPoolSize(2);
+	    executor.setQueueCapacity(500);
+	    executor.setThreadNamePrefix("OrderTask-");
+	    executor.initialize();
+	    return executor;
+	  }
+
 }
